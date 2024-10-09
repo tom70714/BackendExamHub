9e0d7616acfbce7acd4aa0ebc963a42695c1b8af
tom <tom70714@gmail.com>
2024-10-09 17:01:10 +0800

    建立資料庫連線，以及控制器，但測試無法正常CRUD

---
+++ TY.test1/TY.test1/Models/ViewModels/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TY.test1.Models.ViewModels
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public required string AcpdSid { get; set; }
+
+		public string AcpdCname { get; set; }
+
+		public string AcpdEname { get; set; }
+
+		public string AcpdSname { get; set; }
+
+		public string AcpdEmail { get; set; }
+
+		public byte AcpdStatus { get; set; }
+
+		public bool AcpdStop { get; set; }
+
+		public string AcpdStopMemo { get; set; }
+
+		public string AcpdLoginId { get; set; }
+
+		public string AcpdLoginPw { get; set; }
+
+		public string AcpdMemo { get; set; }
+
+		public DateTime AcpdNowdatetime { get; set; }
+
+		public string AppdNowid { get; set; }
+
+		public DateTime AcpdUpddatetitme { get; set; }
+
+		public string AcpdUpdid { get; set; }
+	}
+}
